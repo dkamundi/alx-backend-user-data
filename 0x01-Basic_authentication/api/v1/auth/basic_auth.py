13abb7962d15868b907d9f7aeffe5112d70d0e13
dkamundi <davekamundi@gmail.com>
2023-11-10 00:38:33 +0000

    9. Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -48,3 +48,23 @@ class BasicAuth(Auth):
             return decoded_value
         except base64.binascii.Error:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Extract the user email and password from the Base64 decoded value.
+
+        Return None, None if decoded_base64_authorization_header is None.
+        Return None, None if decoded_base64_authorization_header is not a string.
+        Return None, None if decoded_base64_authorization_header doesn’t contain :.
+        Otherwise, return the user email and the user password - these 2 values must be separated by :.
+        You can assume decoded_base64_authorization_header will contain only one :.
+        """
+        if decoded_base64_authorization_header is None or not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        user_email, user_password = decoded_base64_authorization_header.split(':', 1)
+
+        return user_email, user_password
